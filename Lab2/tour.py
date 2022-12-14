7ea60d6e17cbef45b54462fb8fe799968a2ed06e
Ильин Николай <92156926+ilyinNikolay@users.noreply.github.com>
2022-12-15 00:45:27 +0500

    Add files via upload

---
+++ Lab2/tour.py
@@ -0,0 +1,14 @@
+﻿from route import Route
+import json
+
+class Tour:
+    def __init__(self, nameT = "", routes = []):
+        self.nameT = nameT
+        self.routes = routes
+        self.__dict__["routes"] = [i.__dict__ for i in self.routes] 
+     
+    def __str__(self) -> str:
+        return str(self.__dict__)
+
+    def __repr__(self)-> str:
+        return self.__str__()
